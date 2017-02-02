1af00f647fab138fce39dcd0a5862cb08ac2a4c2
Ant Cosentino <antony@mycosentino.com>
2017-02-02 13:57:33 +0200

    adds new services and message proxying

--- src/init/cluster.js
+++ src/init/cluster.js
@@ -26,7 +26,7 @@ var OUTER = this
 var worker_count = os.cpus().length, worker_ready = 0
 
 // service and cluster references
-var did_service, lifekey
+var did_service, notifier_service, lifekey
 
 function cluster_send(msg) {
   Object.keys(
@@ -52,6 +52,33 @@ function init_did_service(then) {
     if (msg.ready) {
       // notify all workers that did service is available
       cluster_send({did_service_ready: true})
+    } else if (msg.push_notification_request) {
+      notifier_service.send(msg)
+    } else {
+      // otherwise, nothing doing
+    }
+  })
+  if (typeof then === 'function') then()
+}
+
+function init_notifier_service(then) {
+  notifier_service = cp.fork(
+    './src/init/notifier.js'
+  ).on('error', function(err) {
+    console.log('notifier service error', err)
+  }).on('close', function(code, signal) {
+    console.log('notifier service exit', code, signal)
+    // notify all workers that did service is unavailable
+    cluster_send({notifier_service_ready: false})
+    // then attempt to restart the service
+    init_notifier_service.call(OUTER)
+  }).on('message', function(msg) {
+    console.log('notifier service message', msg)
+    if (msg.ready) {
+      // notify all workers that did service is available
+      cluster_send({notifier_service_ready: true})
+    } else {
+      // otherwise, nothing doing
     }
   })
   if (typeof then === 'function') then()
@@ -74,10 +101,14 @@ lifekey = cluster({
             // initialise the did service worker and notify
             // all http workers
             init_did_service.call(OUTER)
+            init_notifier_service.call(OUTER)
           }
         } else if (message.did_allocation_request) {
           // proxy the message to the DID service
           did_service.send(message)
+        } else if (message.push_notification_request || message.webhook_request) {
+          // proxy the message to the notifier service
+          notifier_service.send(message)
         } else {
           // nothing doing, otherwise
         }
