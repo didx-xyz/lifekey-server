1f1f7b890b489f87008d9924beeec2ddb82f7a74
Ant Cosentino <antony@mycosentino.com>
2017-02-06 11:25:38 +0200

    adds initial impln for required headers middleware

---
+++ src/middlewares/ensure-required-headers-present.js
@@ -0,0 +1,25 @@
+
+'use strict'
+
+module.exports = function(req, res, next) {
+
+  // if the current route and method are
+  // not a secured route, skip the middleware
+  if (!this.get(`secure_${req.method.toLowerCase()}_${req.route.path}`)) return next()
+
+  // otherwise, check the headers
+  if (!('x-cnsnt-did' in req.headers &&
+        'x-cnsnt-plain' in req.headers &&
+        'x-cnsnt-signable' in req.headers &&
+        'x-cnsnt-signed' in req.headers)) {
+    // if missing any of the above
+    return res.status(400).json({
+      error: true,
+      status: 400,
+      message: 'authentication parameters missing from request headers',
+      body: null
+    })
+  }
+  // all good, otherwise
+  return next()
+}
