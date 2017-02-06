3436174d6d13c854548ba75c2dcc61da6a3839a3
Ant Cosentino <antony@mycosentino.com>
2017-02-06 11:27:32 +0200

    adds process messaging blocks supporting fluster data messages and regular signals adds middlewares adds secured and activated routes

--- src/init/worker.js
+++ src/init/worker.js
@@ -2,13 +2,31 @@
 'use strict'
 
 process.on('message', function(message) {
+
+  if (Array.isArray(message)) {
+    // to support fluster data messages
+
+    for (var i = 0, len = message.length; i < len; i++) {
+      // process message
+    }
+  }
+
   if (typeof message.did_service_ready === 'boolean') {
     console.log('SLAVE updating DID service availability to', (
       message.did_service_ready ?
-      'AVAILABLE' :
-      'UNAVAILABLE'
+      '[AVAILABLE]' :
+      '[UNAVAILABLE]'
     ))
     server.set('did_service_ready', message.did_service_ready)
+  } else if (typeof message.notifier_service_ready === 'boolean') {
+    console.log('SLAVE updating notifier service availability to', (
+      message.notifier_service_ready ?
+      '[AVAILABLE]' :
+      '[UNAVAILABLE]'
+    ))
+    server.set('notifier_service_ready', message.notifier_service_ready)
+  } else {
+    // otherwise, nothing doing
   }
 })
 
@@ -28,6 +46,12 @@ var morgan = require('morgan')
 var bodyParser = require('body-parser')
 var express = require('express')
 
+var ensureAppActivationLinkClicked = require('../middleware/ensure-app-activation-link-clicked')
+var ensureRequiredHeadersPresent = require('../middlewares/ensure-required-headers-present')
+var mitigateReplayAttack = require('../middlewares/mitigate-replay-attack')
+var verifySignature = require('../middlewares/verify-signature')
+var notFound = require('../middlewares/not-found')
+
 var TESTING = NODE_ENV === 'testing' || !!~(process.env._ || '').indexOf('istanbul')
 
 var server = express()
@@ -46,36 +70,44 @@ require('./database')(
   server.set('db', db)
   server.set('models', models)
   
-  // TODO
-  // mount middlewares here ---
+  // attach app activation middleware
+  server.use(ensureAppActivationLinkClicked.bind(server))
 
+  // attach authentication middleware
+  server.use(ensureRequiredHeadersPresent.bind(server))
+  server.use(mitigateReplayAttack.bind(server))
+  server.use(verifySignature.bind(server))
+
+  // enumerate all routes
   fs.readdir(`${__dirname}/../routes`, function(err, files) {
     if (err) {
       console.log('unable to enumerate routes', err)
       process.exit(1)
     }
+    
+    // load them and attach to express instance
     files.map(function(file) {
       return require(`../routes/${file}`)
     }).reduce(function(prev, curr) {
       return prev.concat(curr)
     }).forEach(function(route) {
+      server.set( // set whether user must have app activated to invoke route
+        `active_${route.method}_${route.uri}`,
+        route.active
+      )
+      server.set( // set whether user must authenticate to invoke route
+        `secure_${route.method}_${route.uri}`,
+        route.secure
+      )
       server[route.method](
         route.uri,
-        route.callback.bind(server) // we dynamically bind to `server` to allow
-                                    // lookups on `this` in route callbacks
+        route.callback.bind(server)
       )
     })
   })
 
   // 404 route
-  server.use(function(err, req, res, next) {
-    res.status(404)
-    return res.json({
-      status: 404,
-      error: true,
-      message: 'not found'
-    })
-  })
+  server.use(notFound.bind(server))
 
   // and finally, attach
   server.listen(env.WEB_PORT, function() {
