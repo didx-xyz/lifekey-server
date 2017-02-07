600a2a9083649f74be6246f346650c37362194ae
Ant Cosentino <antony@mycosentino.com>
2017-02-07 14:44:34 +0200

    add model containing permitted resource routes for data transfer

---
+++ src/models/information_sharing_permission.js
@@ -0,0 +1,18 @@
+
+module.exports = function(instance, sqlize) {
+  return instance.define('information_sharing_permission', {
+    information_sharing_agreement_id: {
+      type: sqlize.INTEGER,
+      allowNull: false
+    },
+    resource_uri: {
+      type: sqlize.TEXT,
+      allowNull: false
+    }
+  }, {
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+    comment: 'information sharing agreements'
+  })
+}
