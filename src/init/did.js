68d86bf60b5df512e4321d3c2955a3316cd9cacc
Ant Cosentino <antony@mycosentino.com>
2017-02-07 23:08:04 +0200

    try and make this a little more realistic

--- src/init/did.js
+++ src/init/did.js
@@ -1,24 +1,25 @@
 
 'use strict'
 
-process.on('message', function(msg) {
-  if (msg.did_allocation_request) {
-    // inspect the envelope
-    // call a smart contract
-    // reply to requesting client
-    // via firebase push
-  }
+function eis_register() {}
+
+require('./database')(false).then(function(database) {
+  
+  var {db, models} = database
+  
+  process.on('message', function(msg) {
+    if (msg.did_allocation_request) {
+      // inspect the envelope
+      // call a smart contract
+      // reply to requesting client
+      // via firebase push or webhook
+    }
+  }).send({ready: true})
+
+  // simulate work for now...
+  setInterval(function() {
+    process.send({now: Date.now()})
+  }, 1000 * 60)
+  
 })
 
-process.send({ready: true})
-
-// simulate work for now...
-setInterval(function() {
-  process.send({now: Date.now()})
-}, 1000 * 60)
-
-setTimeout(function() {
-  // intentionally uncaught referenceerror
-  // to test the service availability code
-  ajsdfjsaldfjsdlfj
-}, 1000 * 60 * 5)
