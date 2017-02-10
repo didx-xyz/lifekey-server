95d5d3ee8fec8320774eafa3ac441141e8407d0d
Ant Cosentino <antony@mycosentino.com>
2017-02-10 10:26:55 +0200

    formatting and request property fix

--- src/middlewares/assert-headers.js
+++ src/middlewares/assert-headers.js
@@ -4,16 +4,20 @@
 module.exports = function(req, res, next) {
 
   // if the matched route is neither secured-only, nor activated-only, skip the middleware
-  var is_secure = this.get(`secure_${req.method.toLowerCase()}_${req.route.path}`)
-  var is_active = this.get(`active_${req.method.toLowerCase()}_${req.route.path}`)
-  if (!(is_active && is_secure)) {
-    req.skip_secure_checks = !is_secure
-    req.skip_active_checks = !is_active
-    return next()
-  }
+  var is_secure = this.get(
+    `secure_${req.method.toLowerCase()}_${req.route.path}`
+  )
+  var is_active = this.get(
+    `active_${req.method.toLowerCase()}_${req.route.path}`
+  )
+
+  req.skip_secure_checks = !is_secure
+  req.skip_active_checks = !is_active
+  if (!(is_active && is_secure)) return next()
 
   // otherwise, check the headers
-  if (!(('x-cnsnt-id' in req.headers || 'x-cnsnt-did' in req.headers) &&
+  if (!(('x-cnsnt-id' in req.headers ||
+         'x-cnsnt-did' in req.headers) &&
         'x-cnsnt-plain' in req.headers &&
         'x-cnsnt-signable' in req.headers &&
         'x-cnsnt-signed' in req.headers)) {
