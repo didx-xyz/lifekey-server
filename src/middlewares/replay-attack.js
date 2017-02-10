87d8684e9cd9a873a92d026a81708b5092187841
Ant Cosentino <antony@mycosentino.com>
2017-02-10 10:26:13 +0200

    refactor replay attack middleware

--- src/middlewares/replay-attack.js
+++ src/middlewares/replay-attack.js
@@ -7,41 +7,17 @@ module.exports = function(req, res, next) {
   if (req.skip_secure_checks) return next()
 
   // load models for querying and insertion
-  var {
-    user,
-    crypto_key,
-    http_request_verification
-  } = this.get('models')
+  var {http_request_verification} = this.get('models')
 
-  var user_public_key
-  
-  // find their http signing key
-  return crypto_key.findOne({
+  var hex_public_key = req.user.crypto.public_key.toString('hex')
+
+  http_request_verification.findOne({
     where: {
-      owner_id: req.user.id,
-      alias: 'client-server-http'
+      public_key: hex_public_key,
+      algorithm: req.user.crypto.algorithm,
+      signable: req.headers['x-cnsnt-signable'],
+      signature: req.headers['x-cnsnt-signed']
     }
-  }).then(function(found) {
-    if (found) {
-      // attach the key for subsequent middleware
-      req.user.crypto = found
-      user_public_key = found.public_key.toString('hex')
-      // ensure previous requests on record
-      // have not used the provided authentication data
-      return http_request_verification.findOne({
-        where: {
-          public_key: user_public_key,
-          signable: req.headers['x-cnsnt-signable'],
-          signature: req.headers['x-cnsnt-signed']
-        }
-      })
-    }
-    return Promise.reject({
-      error: true,
-      status: 404,
-      message: 'crypto key not found',
-      body: null
-    })
   }).then(function(found) {
     if (found) {
       return Promise.reject({
@@ -53,10 +29,10 @@ module.exports = function(req, res, next) {
     }
     // create the record for posterity
     return http_request_verification.create({
-      public_key: user_public_key,
+      public_key: hex_public_key,
       algorithm: req.user.crypto.algorithm,
       signable: req.headers['x-cnsnt-signable'],
-      signed: req.headers['x-cnsnt-signed']
+      signature: req.headers['x-cnsnt-signed']
     })
   }).then(function(created) {
     if (created) return next() // we're done here!
