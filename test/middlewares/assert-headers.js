e19bcf9c853972e168b6d87d433211bb038fb290
Ant Cosentino <antony@mycosentino.com>
2017-02-10 10:25:29 +0200

    add tests for assert headers middleware

--- test/middlewares/assert-headers.js
+++ test/middlewares/assert-headers.js
@@ -1,28 +1,70 @@
 
 'use strict'
 
-var crypto = require('crypto')
-
-var ec = require('eccrypto')
 var {expect} = require('chai')
 
-// mock express instance
 var mock = require('../mock/express')
-
-// the test subject
 var subject = require('../../src/middlewares/assert-headers')
 
-describe.skip('assert headers', function() {
-  before(function(done) {
-    done(new Error('not implemented'))
+before(function(done) {
+  mock.express.set('secure_foo_foo', false)
+  mock.express.set('active_foo_foo', false)
+  mock.express.set('secure_foo_baz', true)
+  mock.express.set('active_foo_baz', true)
+  done()
+})
+
+describe('middleware assert-headers', function() {
+
+  it('should invoke the success callback if request is skippable', function(done) {
+    subject.call(mock.express, {
+      method: 'FOO',
+      route: {path: 'foo'}
+    }, done.bind(
+      done, new Error('should not be called')
+    ), done)
   })
 
-  describe('middleware assert-headers', function() {
-    it('should respond with error if required headers are missing', function(done) {
-      done(new Error('not implemented'))
-    })
-    it('should invoke the success callback if required headers are present', function(done) {
-      done(new Error('not implemented'))
+  it('should respond with error if required headers are missing', function(done) {
+    subject.call(mock.express, {
+      method: 'FOO',
+      route: {path: 'baz'},
+      headers: {}
+    }, mock.res(function(res) {
+      expect(res.error).to.equal(true)
+      expect(res.status).to.equal(400)
+      expect(res.message).to.equal('authentication parameters missing from request headers')
+      done()
+    }), done.bind(
+      done, new Error('should not be called')
+    ))
+  })
+  
+  it('should invoke the success callback if required headers are present', function(done) {
+    subject.call(mock.express, {
+      method: 'FOO',
+      route: {path: 'baz'},
+      headers: {
+        'x-cnsnt-id': 'foo',
+        'x-cnsnt-plain': 'foo',
+        'x-cnsnt-signable': 'foo',
+        'x-cnsnt-signed': 'foo'
+      }
+    }, done.bind(
+      done, new Error('should not be called')
+    ), function() {
+      subject.call(mock.express, {
+        method: 'FOO',
+        route: {path: 'baz'},
+        headers: {
+          'x-cnsnt-did': 'foo',
+          'x-cnsnt-plain': 'foo',
+          'x-cnsnt-signable': 'foo',
+          'x-cnsnt-signed': 'foo'
+        }
+      }, done.bind(
+        done, new Error('should not be called')
+      ), done)
     })
   })
 })
