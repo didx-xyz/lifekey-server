9b79d4c44023f9c58be7a5a481aee1ff1e3a0637
Ant Cosentino <antony@mycosentino.com>
2017-02-10 10:24:38 +0200

    add tests for replay attack middleware

--- test/middlewares/replay-attack.js
+++ test/middlewares/replay-attack.js
@@ -1,39 +1,62 @@
 
 'use strict'
 
-var crypto = require('crypto')
-
-var ec = require('eccrypto')
 var {expect} = require('chai')
 
-// mock express instance
 var mock = require('../mock/express')
-
-// the test subject
 var subject = require('../../src/middlewares/replay-attack')
 
-describe.skip('replay attack', function() {
-  before(function(done) {
-    // TODO create fixtures for each test case
-    done(new Error('not implemented'))
+var now = Date.now()
+
+describe('middleware replay-attack', function() {
+  
+  it('should invoke the success callback if the request is skippable', function(done) {
+    subject.call(mock.express, {
+      skip_secure_checks: true
+    }, done.bind(
+      done, new Error('should not have been called')
+    ), done)
   })
 
-  describe('middleware replay-attack', function() {
-    
-    it('should respond with not found if unable to find the specified user record', function(done) {
-      done(new Error('not implemented'))
-    })
-
-    it('should respond with not found if unable to to find the specified crypto_key record', function(done) {
-      done(new Error('not implemented'))
-    })
-
-    it('should respond with error if a known signature is detected', function(done) {
-      done(new Error('not implemented'))
-    })
-
-    it('should invoke the success callback if the signature was logged to database', function(done) {
-      done(new Error('not implemented'))
-    })
+  it('should invoke the success callback if the signature was logged to database', function(done) {
+    subject.call(mock.express, {
+      skip_secure_checks: false,
+      headers: {
+        'x-cnsnt-signable': now,
+        'x-cnsnt-signed': now
+      },
+      user: {
+        crypto: {
+          public_key: Buffer.from('foo'),
+          algorithm: 'foo'
+        }
+      }
+    }, done.bind(
+      done, new Error('should not have been called')
+    ), done)
   })
+
+  it('should respond with error if a known signature is detected', function(done) {
+    subject.call(mock.express, {
+      skip_secure_checks: false,
+      headers: {
+        'x-cnsnt-signable': now,
+        'x-cnsnt-signed': now
+      },
+      user: {
+        crypto: {
+          public_key: Buffer.from('foo'),
+          algorithm: 'foo'
+        }
+      }
+    }, mock.res(function(res) {
+      expect(res.error).to.equal(true)
+      expect(res.status).to.equal(400)
+      expect(res.message).to.equal('detected a known signature')
+      done()
+    }), done.bind(
+      done, new Error('should not have been called')
+    ))
+  })
+
 })
