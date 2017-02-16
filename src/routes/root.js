9fafe250afa3547998173302c89fde321cf5cbb7
Ant Cosentino <antony@mycosentino.com>
2017-02-16 10:39:59 +0200

    add index route

---
+++ src/routes/root.js
@@ -0,0 +1,17 @@
+
+module.exports = [
+  {
+    uri: '/',
+    method: 'get',
+    secure: false,
+    active: false,
+    callback: function(req, res) {
+      return res.status(200).json({
+        error: false,
+        status: 200,
+        message: 'sorry, there\'s nothing to see here',
+        body: null
+      })
+    }
+  }
+]
