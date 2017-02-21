c8bd359b226cc42e422a49f83324dce827801ed9
Ant Cosentino <antony@mycosentino.com>
2017-02-21 11:57:07 +0200

    notification service update - add fcm and webhooks and retries

--- src/init/notifier.js
+++ src/init/notifier.js
@@ -1,8 +1,24 @@
 
 'use strict'
 
-var http = require('https')
+var url = require('url')
+
 var fcm = require('../messaging/fcm')
+var webhook = require('../messaging/webhook')
+
+var failures = {
+  fcm: [],
+  webhook: []
+}
+
+function retryonsent(err) {
+  var retry = failures[this.arr][this.idx]
+  if (err) {
+    retry.ttl -= 1
+  } else {
+    failures[this.arr].splice(this.idx, 1)
+  }
+}
 
 require('./database')(
   false // disable logging
@@ -11,13 +27,67 @@ require('./database')(
   var {db, models} = database
 
   process.on('message', function(msg) {
+
     if (msg.push_notification_request) {
-      // fcm(...)
+      
+      var {user_id, device_id, notification, data} = msg.push_notification_request
+      // FIXME wire up to retries (has prerequisite FIXME in messaging/fcm)
+      fcm(device_id, notification, data, console.log)
     } else if (msg.webhook_notification_request) {
-      // http(...)
+      
+      var {user_id, webhook_url, notification, data} = msg.webhook_notification_request
+      var msg = {notification: notification, data: data}
+
+      var get_user_hook_addr = (function() {
+        if (webhook_url) return Promise.resolve(url.parse(webhook_url))
+        return database.models.user.findOne({
+          where: {id: user_id}
+        }).then(function(found) {
+          if (found && found.webhook_url) {
+            return Promise.resolve(
+              url.parse(found.webhook_url)
+            )
+          } else if (found && !found.webhook_url) {
+            return Promise.reject(
+              {webhook_err: 'user is not enrolled for webhook notifications'}
+            )
+          } else {
+            return Promise.reject(
+              {webhook_err: 'user record not found'}
+            )
+          }
+        })
+      })()
+
+      get_user_hook_addr.then(function(addr) {
+        webhook(addr, msg, function(err) {
+          if (err) {
+            failures.webhook.push({
+              uri: addr,
+              msg: msg,
+              ttl: 9
+            })
+          }
+        })
+      }).catch(function(err) {
+        // fatal error, cannot retry
+        // (could happen if user no longer exists)
+        console.log(err)
+      })
+
     } else {
       // otherwise, nothing doing
     }
   }).send({ready: true})
 
 })
+
+var retryTimer = setInterval(function() {
+  // TODO fcm retries
+  
+  for (var i = failures.webhook.length - 1; i >= 0; i--) {
+    var icur = failures.webhook[i]
+    if (icur.ttl === 0) failures.webhook.splice(i, 1)
+    webhook(icur.uri, icur.msg, retryonsent.bind({arr: 'webhook', idx: i}))
+  }
+}, 20 * 1000)
