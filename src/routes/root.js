2eb12fbbc36019af2bddc0b9a6516c8d32d8a3f6
Ant Cosentino <antony@mycosentino.com>
2017-02-24 14:03:49 +0200

    add route for logging

--- src/routes/root.js
+++ src/routes/root.js
@@ -23,5 +23,24 @@ module.exports = [
       res.set('content-type', 'text/plain')
       return res.status(200).end('User-agent: *\nDisallow: /')
     }
+  },
+  {
+    // /azenv.php?auth=148793672741&a=PSCMN&i=2194886317&p=80
+    uri: '/azenv.php',
+    method: 'get',
+    secure: false,
+    active: false,
+    callback: function(req, res) {
+      
+      var {auth, a, i, p} = req.query
+      console.log(req.headers)
+      res.status(200).json({
+        who: 'are',
+        you: 'and',
+        how: 'did',
+        you: 'get',
+        this: 'number?'
+      })
+    }
   }
 ]
