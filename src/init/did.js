d01cc4bcbc95cd76283f697caa08feac40a0a1a6
Ant Cosentino <antony@mycosentino.com>
2017-04-07 15:53:36 +0200

    shuffle the event bindings around add log message for did assignment

--- src/init/did.js
+++ src/init/did.js
@@ -9,50 +9,48 @@ var eu = require('ethereumjs-util')
 var env = require('./env')()
 var isw = require('identity-service-wrapper')(env.EIS_HOST)
 
-var registrants = {}
-
-isw.registry.CreatedDID(function(err, event) {
-  if (err) return console.log('EIS created_did event error', err)
-  var {did, sender, owner, admin, ddo} = event.args
-  if (!(owner in registrants)) return
-  var {user_id, ddo} = registrants[owner]
-  delete registrants[owner]
-  user.update({
-    did_address: did,
-    did: ddo
-  }, {
-    where: {id: user_id}
-  }).then(function(updated) {
-    if (!updated[0]) {
-      return console.log('EIS db update error', 'unable to update user record')
-    }
-    process.send({
-      notification_request: {
-        type: 'received_did',
-        user_id: user_id,
-        data: {
-          type: 'received_did',
-          received_did: true,
-          did_value: ddo,
-          did_address: did
-        },
-        notification: {
-          title: 'You have been allocated a decentralised identifier',
-          body: 'Click here to view your DID!'
-        }
-      }
-    })
-  }).catch(console.log)
-})
-
 require('./database')(false).then(function(database) {
 
+  var registrants = {}
   var {user, crypto_key} = database.models
+
+  isw.registry.CreatedDID(function(err, event) {
+    if (err) return console.log('EIS created_did event error', err)
+    var {did, sender, owner, admin, ddo} = event.args
+    if (!(owner in registrants)) return
+    var {user_id, ddo} = registrants[owner]
+    delete registrants[owner]
+    user.update({
+      did_address: did,
+      did: ddo
+    }, {
+      where: {id: user_id}
+    }).then(function(updated) {
+      if (!updated[0]) {
+        return console.log('EIS db update error', 'unable to update user record')
+      }
+      console.log('EIS ddo update success', user_id)
+      process.send({
+        notification_request: {
+          type: 'received_did',
+          user_id: user_id,
+          data: {
+            type: 'received_did',
+            received_did: true,
+            did_value: ddo,
+            did_address: did
+          },
+          notification: {
+            title: 'You have been allocated a decentralised identifier',
+            body: 'Click here to view your DID!'
+          }
+        }
+      })
+    }).catch(console.log)
+  })
   
   process.on('message', function(msg) {
-
     if (!msg.did_allocation_request) return
-    
     var {user_id} = msg.did_allocation_request
     
     crypto_key.findOne({
@@ -76,7 +74,6 @@ require('./database')(false).then(function(database) {
         if (err) {
           return console.log('EIS spawn error', err)
         }
-
         registrants[user_address] = {
           ddo: did_value,
           user_id: user_id
