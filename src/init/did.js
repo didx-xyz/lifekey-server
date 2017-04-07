b57bb9214fe784bd5d00ab573ac1a7276e54c1f3
Ant Cosentino <antony@mycosentino.com>
2017-04-07 15:21:08 +0200

    change confirmation process to use contract events

--- src/init/did.js
+++ src/init/did.js
@@ -9,6 +9,41 @@ var eu = require('ethereumjs-util')
 var env = require('./env')()
 var isw = require('identity-service-wrapper')(env.EIS_HOST)
 
+var registrants = {}
+
+isw.registry.CreatedDID(function(err, event) {
+  var {did, sender, owner, admin, ddo} = event.args
+  if (!(owner in registrants)) return
+  var {user_id, ddo} = registrants[owner]
+  delete registrants[owner]
+  user.update({
+    did_address: did,
+    did: ddo
+  }, {
+    where: {id: user_id}
+  }).then(function(updated) {
+    if (!updated[0]) {
+      return console.log('EIS db update error', 'unable to update user record')
+    }
+    process.send({
+      notification_request: {
+        type: 'received_did',
+        user_id: user_id,
+        data: {
+          type: 'received_did',
+          received_did: true,
+          did_value: ddo,
+          did_address: did
+        },
+        notification: {
+          title: 'You have been allocated a decentralised identifier',
+          body: 'Click here to view your DID!'
+        }
+      }
+    })
+  }).catch(console.log)
+})
+
 require('./database')(false).then(function(database) {
 
   var {user, crypto_key} = database.models
@@ -31,61 +66,20 @@ require('./database')(false).then(function(database) {
 
       var admin_address = `0x${eu.privateToAddress(Buffer.from(env.EIS_ADMIN_KEY, 'hex')).toString('hex')}`
       var user_address = `0x${eu.privateToAddress(Buffer.from(found.private_key, 'hex')).toString('hex')}`
+
+      // generate a did value
+      var did_value = crypto.rng(32).toString('hex')
       
-      // create the did contract
-      isw.spawn(admin_address, user_address, env.EIS_SIGNER_KEY, function(err, skip, receipt_logs) {
+      // create the did contract with initial ddo
+      isw.spawn(admin_address, user_address, env.EIS_SIGNER_KEY, did_value, function(err, txhash) {
         if (err) {
           return console.log('EIS spawn error', err)
         }
-        var did_addr = '0x' + receipt_logs[0].data.slice(26).slice(0, 40)
 
-        // generate a did value
-        var did_value = crypto.rng(32).toString('hex')
-
-        // send the did value to the did contract
-        isw.update(did_addr, did_value, env.EIS_ADMIN_KEY, function(err, updated, receipt_logs) {
-          if (err) return console.log('EIS update error', err)
-
-          // use the did address to check if the value
-          // we just sent matches the one we generated
-          isw.verify(did_addr, function(err, res) {
-            if (err) {
-              return console.log('EIS verify error', err)
-            } else if (res === did_value) {
-              // update user record with contract address and did value
-              user.update({
-                did_address: did_addr,
-                did: did_value
-              }, {
-                where: {id: user_id}
-              }).then(function(updated) {
-                if (!updated[0]) {
-                  return console.log('EIS db update error', 'unable to update user record')
-                }
-                
-                process.send({
-                  notification_request: {
-                    type: 'received_did',
-                    user_id: user_id,
-                    data: {
-                      type: 'received_did',
-                      received_did: true,
-                      did_value: did_value,
-                      did_address: did_addr
-                    },
-                    notification: {
-                      title: 'You have been allocated a decentralised identifier',
-                      body: 'Click here to view your DID!'
-                    }
-                  }
-                })
-              }).catch(console.log)
-            } else {
-              // values don't match
-              console.log('EIS verify error', 'did contract update failed', res, did_value)
-            }
-          })
-        })
+        registrants[user_address] = {
+          ddo: did_value,
+          user_id: user_id
+        }
       })
     }).catch(console.log)
   })
