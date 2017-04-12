ae02ec71d61e27d43fb7073353e00197b46fbd51
Ant Cosentino <antony@mycosentino.com>
2017-04-12 12:08:41 +0200

    check that the contract object initialised successfully, and conditionally serve. add check for did value.

--- src/routes/management.js
+++ src/routes/management.js
@@ -18,19 +18,21 @@ var cuid = require('cuid')
 
 var our_crypto = require('../crypto')
 
+var thanks_balance_check_available = true
+
 var THANKS_TOKEN_CONTRACT_ABI = [ { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string", "value": "Thanks" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256", "value": "1000000000" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8", "value": "0" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "standard", "outputs": [ { "name": "", "type": "string", "value": "Token 0.1" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string", "value": "TNX" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }, { "name": "_extraData", "type": "bytes" } ], "name": "approveAndCall", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" }, { "name": "", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "type": "function" }, { "inputs": [ { "name": "initialSupply", "type": "uint256", "index": 0, "typeShort": "uint", "bits": "256", "displayName": "initial Supply", "template": "elements_input_uint", "value": "1000000000" }, { "name": "tokenName", "type": "string", "index": 1, "typeShort": "string", "bits": "", "displayName": "token Name", "template": "elements_input_string", "value": "Thanks" }, { "name": "decimalUnits", "type": "uint8", "index": 2, "typeShort": "uint", "bits": "8", "displayName": "decimal Units", "template": "elements_input_uint", "value": "0" }, { "name": "tokenSymbol", "type": "string", "index": 3, "typeShort": "string", "bits": "", "displayName": "token Symbol", "template": "elements_input_string", "value": "TNX" } ], "payable": false, "type": "constructor" }, { "payable": false, "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ]
 
 try {
   var w3 = new web3(new web3.providers.HttpProvider(env.EIS_HOST))
+  var thanks = w3.eth.contract(
+    THANKS_TOKEN_CONTRACT_ABI
+  ).at(
+    env.THANKS_TOKEN_CONTRACT_ADDRESS
+  )
 } catch (e) {
+  thanks_balance_check_available = false
   console.log('unable to initialise connection to eis host')
 }
-
-var thanks = w3.eth.contract(
-  THANKS_TOKEN_CONTRACT_ABI
-).at(
-  env.THANKS_TOKEN_CONTRACT_ADDRESS
-)
 
 module.exports = [
   
@@ -1936,24 +1938,46 @@ module.exports = [
     method: 'get',
     secure: false,
     active: false,
-    callback: function(req, res) {
-      thanks.balanceOf(did, function(err, res) {
-        if (err) {
-          return res.status(500).json({
+    callback: thanks_balance_check_available ? (
+      function(req, res) {
+        var {did} = req.params
+        if (!(typeof did === 'string' &&
+              did.length === 64)) {
+          return res.status(400).json({
             error: true,
-            status: 500,
-            message: 'internal server error',
-            body: {error: err}
+            status: 400,
+            message: 'invalid did value given',
+            body: null
           })
         }
-        return res.status(200).json({
-          error: false,
-          status: 200,
-          message: 'ok',
-          body: {balance: res.toString(10)}
+        thanks.balanceOf(did, function(err, res) {
+          if (err) {
+            console.log('unable to query balance of user', did, err)
+            return res.status(500).json({
+              error: true,
+              status: 500,
+              message: 'internal server error',
+              body: null
+            })
+          }
+          return res.status(200).json({
+            error: false,
+            status: 200,
+            message: 'ok',
+            body: {balance: res.toString(10)}
+          })
         })
-      })
-    }
+      }
+    ) : (
+      function(res, res) {
+        return res.status(500).json({
+          error: true,
+          status: 500,
+          message: 'internal server error',
+          body: null
+        })
+      }
+    )
   },
 
   // 17 POST /management/key
