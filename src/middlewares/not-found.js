6f2da6ecd04c434e75f4abdf5f46a778e6b5229f
Ant Cosentino <antony@mycosentino.com>
2017-04-13 10:59:55 +0200

    add error output if it's there

--- src/middlewares/not-found.js
+++ src/middlewares/not-found.js
@@ -2,6 +2,7 @@
 'use strict'
 
 module.exports = function(err, req, res, next) {
+  if (err) console.log(err)
   console.log('REQUEST DUMP', req.method, req.headers)
   return res.status(404).json({
     status: 404,
