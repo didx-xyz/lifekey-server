bc1a06af8a77d2d89a6b9d7b24f232972ecc9460
Ant Cosentino <antony@mycosentino.com>
2017-04-24 20:03:20 +0200

    add model for liveness

---
+++ src/models/active_bot.js
@@ -0,0 +1,20 @@
+
+'use strict'
+
+module.exports = function(instance, sqlize) {
+  return instance.define('active_bot', {
+    owner_id: {
+      type: sqlize.INTEGER,
+      allowNull: false
+    },
+    last_ping: {
+      type: sqlize.DATE,
+      allowNull: false
+    }
+  }, {
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+    comment: 'liveness checking for bot users'
+  })
+}
