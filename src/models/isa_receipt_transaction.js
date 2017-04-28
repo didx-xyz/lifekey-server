66766c6ec133bda81a3d078747f78f1cfb1f3f3f
Ant Cosentino <antony@mycosentino.com>
2017-04-28 18:02:05 +0200

    new database model for ledgerings

---
+++ src/models/isa_receipt_transaction.js
@@ -0,0 +1,22 @@
+
+module.exports = function(instance, sqlize) {
+  return instance.define('isa_receipt_transaction', {
+    isa_id: {
+      type: sqlize.INTEGER,
+      allowNull: true
+    },
+    receipt_hash: {
+      type: sqlize.STRING,
+      allowNull: false
+    },
+    transaction_hash: {
+      type: sqlize.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+  })
+}
