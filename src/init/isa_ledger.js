7bb22dd636103b427a392014b8faafd689fb2fb4
Ant Cosentino <antony@mycosentino.com>
2017-06-19 15:09:16 +0200

    flatten out promise pipeline

--- src/init/isa_ledger.js
+++ src/init/isa_ledger.js
@@ -12,11 +12,8 @@ var env = require('./env')()
 var get_receipt = require('../routes/management')[23]
 var mock = require('../../test/mock/express')
 
-// service ready state
-var error = false
-
 // forward references
-var db, models, w3, all_time, nonces = {}
+var db, models, w3, private_key, addr, all_time, nonces = {}
 
 function get_next_nonce(address, callback) {
   if (typeof nonces[address] === 'undefined') {
@@ -105,9 +102,10 @@ require('./database')(
     w3 = new web3(new web3.providers.HttpProvider(env.EIS_HOST))
   } catch (e) {
     console.log('unable to initialise web3 instance to eis host', e)
-    process.send({ready: false})
-    return
+    return Promise.reject(e)
   }
+  return Promise.resolve()
+}).then(function() {
   w3.net.peerCount(console.log.bind(console, 'net.peerCount'))
   w3.net.listening(console.log.bind(console, 'net.listening'))
   w3.currentProvider(console.log.bind(console, 'currentProvider'))
@@ -116,147 +114,152 @@ require('./database')(
   w3.version.network(console.log.bind(console, 'version.network'))
   w3.version.node(console.log.bind(console, 'version.node'))
   w3.version.api(console.log.bind(console, 'version.api'))
-
+  return Promise.resolve()
+}).then(function() {
   // initialise keys and address
-  var private_key = Buffer.from(env.ISA_RECEIPT_KEY, 'hex')
-  var addr = `0x${ut.privateToAddress(private_key).toString('hex')}`
+  private_key = Buffer.from(env.ISA_RECEIPT_KEY, 'hex')
+  addr = `0x${ut.privateToAddress(private_key).toString('hex')}`
+  return new Promise(function(resolve, reject) {
+    // ensure balance is not zero
+    w3.eth.getBalance(addr, function(err, balance) {
+      if (err) {
+        console.log('error getting balance for ISA_RECEIPT_KEY account', err)
+        return reject(err)
+      } else if (!balance) {
+        console.log('error getting balance for ISA_RECEIPT_KEY account', 'balance is not defined')
+        return reject()
+      } else if (balance.toNumber() <= 0) {
+        console.log('ISA_RECEIPT_KEY account balance too low to continue')
+        return reject()
+      } else {
+        // no error that we can identify
+        return resolve()
+      }
+    })
+  })
+}).then(function() {
+  // find any existing receipt ledgerings
+  return models.isa_receipt_transaction.findAll()
+}).then(function(found) {
+  all_time = {}
+  found.map(function(irt) {
+    return irt.isa_id
+  }).forEach(function(id) {
+    all_time[id] = true
+  })
+  return Promise.resolve()
+}).then(function() {
 
-  // ensure balance is not zero
-  w3.eth.getBalance(addr, function(err, balance) {
-    if (err) {
-      error = true
-      console.log('error getting balance for ISA_RECEIPT_KEY account', err)
-    } else if (!balance) {
-      error = true
-      console.log('error getting balance for ISA_RECEIPT_KEY account', 'balance is not defined')
-    } else if (balance.toNumber() <= 0) {
-      error = true
-      console.log('ISA_RECEIPT_KEY account balance too low to continue')
-    } else {
-      // no error that we can identify
+  // register service message handler
+  process.on('message', function(msg) {
+
+    var {isa_id} = msg.isa_ledger_request
+    
+    if (isa_id in all_time) {
+      return console.log(
+        'skipping receipt ledgering for',
+        isa_id,
+        'receipt ledgering either in progress or already ledgered'
+      )
     }
 
-    // find any existing receipt ledgerings
-    models.isa_receipt_transaction.findAll().then(function(found) {
-      all_time = {}
-      found.map(function(irt) {
-        return irt.isa_id
-      }).forEach(function(id) {
-        all_time[id] = true
-      })
-      return Promise.resolve()
-    }).then(function() {
+    // LOCK
+    all_time[isa_id] = true
 
-      // register service message handler
-      process.on('message', function(msg) {
-
-        var {isa_id} = msg.isa_ledger_request
+    get_receipt.callback.call(mock.express, {
+      skip_relation_check: true,
+      params: {isa_id: isa_id}
+    }, mock.res(function(res) {
+      if (res.error) {
         
-        if (isa_id in all_time) {
+        // UNLOCK
+        delete all_time[isa_id]
+        
+        return console.log(
+          'error calling GET /management/receipt/:isa_id',
+          res
+        )
+      }
+      var receipt_hash = crypto.createHash(
+        'sha256'
+      ).update(
+        Buffer.from(
+          JSON.stringify(res.body),
+          'utf8'
+        )
+      ).digest('base64')
+
+      get_next_nonce(addr, function(err, nonce) {
+        if (err) {
+
+          // UNLOCK
+          delete all_time[isa_id]
+
           return console.log(
-            'skipping receipt ledgering for',
+            'error getting nonce for isa receipt ledgering account',
+            err
+          )
+        }
+        try {
+          var transaction = new tx({
+            to: addr,
+            from: addr,
+            gasLimit: 3000000,
+            gasPrice: +w3.toWei(20, 'gwei'),
+            data: receipt_hash,
+            nonce: nonce,
+            value: 0
+          })
+          transaction.sign(private_key)
+        } catch (e) {
+          
+          // UNLOCK
+          delete all_time[isa_id]
+          
+          return console.log(
+            'error generating or signing raw transaction for isa',
             isa_id,
-            'receipt ledgering either in progress or already ledgered'
+            e
           )
         }
 
-        // LOCK
-        all_time[isa_id] = true
-
-        get_receipt.callback.call(mock.express, {
-          skip_relation_check: true,
-          params: {isa_id: isa_id}
-        }, mock.res(function(res) {
-          if (res.error) {
-            
-            // UNLOCK
-            delete all_time[isa_id]
-            
-            return console.log(
-              'error calling GET /management/receipt/:isa_id',
-              res
-            )
-          }
-          var receipt_hash = crypto.createHash(
-            'sha256'
-          ).update(
-            Buffer.from(
-              JSON.stringify(res.body),
-              'utf8'
-            )
-          ).digest('base64')
-
-          get_next_nonce(addr, function(err, nonce) {
-            if (err) {
-
-              // UNLOCK
-              delete all_time[isa_id]
-
-              return console.log(
-                'error getting nonce for isa receipt ledgering account',
-                err
+        try {
+          w3.eth.sendRawTransaction(
+            `0x${transaction.serialize().toString('hex')}`,
+            function(err, txhash) {
+              if (err) {
+                
+                // UNLOCK
+                delete all_time[isa_id]
+                
+                return console.log('error sending txn for', isa_id, err)
+              }
+              receipts[txhash] = {
+                isa_id: isa_id,
+                receipt_hash: receipt_hash
+              }
+              console.log(
+                'awaiting txn confirmations for isa',
+                isa_id
               )
             }
-            try {
-              var transaction = new tx({
-                to: addr,
-                from: addr,
-                gasLimit: 3000000,
-                gasPrice: +w3.toWei(20, 'gwei'),
-                data: receipt_hash,
-                nonce: nonce,
-                value: 0
-              })
-              transaction.sign(private_key)
-            } catch (e) {
-              
-              // UNLOCK
-              delete all_time[isa_id]
-              
-              return console.log(
-                'error generating or signing raw transaction for isa',
-                isa_id,
-                e
-              )
-            }
-
-            try {
-              w3.eth.sendRawTransaction(
-                `0x${transaction.serialize().toString('hex')}`,
-                function(err, txhash) {
-                  if (err) {
-                    
-                    // UNLOCK
-                    delete all_time[isa_id]
-                    
-                    return console.log('error sending txn for', isa_id, err)
-                  }
-                  receipts[txhash] = {
-                    isa_id: isa_id,
-                    receipt_hash: receipt_hash
-                  }
-                  console.log(
-                    'awaiting txn confirmations for isa',
-                    isa_id
-                  )
-                }
-              )
-            } catch (e) {
-              
-              // UNLOCK
-              delete all_time[isa_id]
-              
-              return console.log(
-                'error serialising transaction for raw send for isa',
-                isa_id,
-                e
-              )
-            }
-          })
-        }))
+          )
+        } catch (e) {
+          
+          // UNLOCK
+          delete all_time[isa_id]
+          
+          return console.log(
+            'error serialising transaction for raw send for isa',
+            isa_id,
+            e
+          )
+        }
       })
-
-      process.send({ready: !error})
-    }).catch(console.log)
+    }))
   })
+
+  process.send({ready: true})
+}).catch(function(err) {
+  process.send({ready: false})
 })
