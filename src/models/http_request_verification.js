2aff631f046435e91ba74823cba9716464057074
Ant Cosentino <antony@mycosentino.com>
2017-06-20 20:22:15 +0200

    adds dubious indices

--- src/models/http_request_verification.js
+++ src/models/http_request_verification.js
@@ -18,6 +18,50 @@ module.exports = function(instance, sqlize) {
       allowNull: false
     }
   }, {
+    indexes: [
+      {
+        name: 'where_index_1',
+        method: 'BTREE',
+        fields: [
+          {
+            attribute: 'signature',
+            order: 'ASC',
+            length: 50
+          },
+          {
+            attribute: 'algorithm',
+            order: 'ASC',
+            length: 20
+          }
+        ]
+      },
+      {
+        name: 'where_index_2',
+        method: 'BTREE',
+        fields: [
+          {
+            attribute: 'public_key',
+            order: 'ASC',
+            length: 50
+          },
+          {
+            attribute: 'algorithm',
+            order: 'ASC',
+            length: 20
+          },
+          {
+            attribute: 'plaintext',
+            order: 'ASC',
+            length: 50
+          },
+          {
+            attribute: 'signature',
+            order: 'ASC',
+            length: 50
+          }
+        ]
+      }
+    ],
     timestamps: true,
     paranoid: true,
     underscored: true,
