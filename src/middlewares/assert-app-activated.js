553be549835fc763f197d3b810458008cd91104d
Ant Cosentino <antony@mycosentino.com>
2017-07-21 13:30:24 +0200

    clear up conditionals with some assignments

--- src/middlewares/assert-app-activated.js
+++ src/middlewares/assert-app-activated.js
@@ -9,15 +9,23 @@ var replayAttack = (
 
 module.exports = function(req, res, next) {
 
-  if ((req.skip_active_checks && req.skip_secure_checks)) {
-    return next()
-  } else if ((req.skip_active_checks && !req.skip_secure_checks)) {
-    return replayAttack.call(this, req, res, next)
-  } else if (!(req.skip_active_checks && req.skip_secure_checks) && req.user.app_activation_link_clicked) {
-    return replayAttack.call(this, req, res, next)
-  } else if (!req.skip_active_checks && req.skip_secure_checks && req.user.app_activation_link_clicked) {
-    return next()
-  } else if (!(req.skip_active_checks && req.user.app_activation_link_clicked)) {
+  var skip_active = req.skip_active_checks
+  var skip_secure = req.skip_secure_checks
+  var skip_all = skip_active && skip_secure
+  var skip_neither = !(skip_active && skip_secure)
+  var is_activated = req.user.app_activation_link_clicked
+
+  if (
+    (!skip_active && skip_secure && is_activated) ||
+    skip_all
+  ) return next()
+
+  if (
+    (skip_active && !skip_secure) ||
+    (skip_neither && is_activated)
+  ) return replayAttack.call(this, req, res, next)
+
+  if (!(skip_active && is_activated)) {
     return res.status(400).json({
       error: true,
       status: 400,
