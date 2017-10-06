2d006698766af9edd39415736cae2773504200dd
Ant Cosentino <antony@mycosentino.com>
2017-10-06 12:28:40 +0200

    upsert the record

--- src/routes/directory.js
+++ src/routes/directory.js
@@ -60,17 +60,19 @@ module.exports = [
       var {active_bot} = this.get('models')
       var errors = this.get('db_errors')
       console.log('bot ping from', req.user.nickname, 'at', req.headers['x-real-ip'])
+
       req.user.update({
         host_address: req.headers['x-real-ip']
       }, {where: {
         owner_id: req.user.id
       }}).then(function() {
-        return active_bot.update({
+        return active_bot.upsert({
           last_ping: new Date
         }, {where: {
           owner_id: req.user.id
         }})
-      }).then(function(updated) {
+      }).then(function(upserted) {
+        console.log('upsert', upserted)
         return res.status(200).json({
           error: false,
           status: 200,
